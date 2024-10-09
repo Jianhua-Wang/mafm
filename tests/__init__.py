9244cb81e9570a8526cad71ce7d227035ce37535
Jianhua-Wang <jianhua.mert@gmail.com>
2024-10-08 23:12:53 -0400

    git init

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,19 +1,1 @@
 """Unit test package for mafm."""
-
-"""Top-level package for easy_finemap."""
-
-import logging
-
-from rich.logging import RichHandler
-
-__author__ = """Jianhua Wang"""
-__email__ = "jianhua.mert@gmail.com"
-__version__ = "0.0.1"
-
-
-logging.basicConfig(
-    level=logging.WARNING,
-    format="%(name)s - %(message)s",
-    datefmt="[%X]",
-    handlers=[RichHandler(rich_tracebacks=True, show_path=False)],
-)
