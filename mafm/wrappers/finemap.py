947e4e81e3ff935be7ad0d5ed57678640cf1baeb
Jianhua-Wang <jianhua.mert@gmail.com>
2024-11-01 11:42:27 -0400

    finemap

--- mafm/wrappers/finemap.py
+++ mafm/wrappers/finemap.py
@@ -2,141 +2,213 @@
 
 import logging
 import os
-import subprocess
-from typing import List, Optional, Union
+from typing import Optional
+
+import numpy as np
+import pandas as pd
 
 from mafm.constants import ColName
+from mafm.credibleset import CredibleSet, combine_creds
 from mafm.mafm import FmInput
 from mafm.utils import io_in_tempdir, tool_manager
 
 logger = logging.getLogger("FINEMAP")
 
 
-def run_finemap(
-    mfinput: FmInput,
-    subprogram: str = "--sss",
-    cond_pvalue: float = 5e-8,
-    corr_config: float = 0.95,
-    force_n_samples: bool = False,
-    n_causal_snps: int = 5,
-    n_configs_top: int = 50000,
-    n_conv_sss: int = 100,
+@io_in_tempdir("./tmp/FINEMAP")
+def run_finemap_single(
+    input: FmInput,
+    max_causal: int = 1,
+    coverage: float = 0.95,
     n_iter: int = 100000,
     n_threads: int = 1,
-    prior_k: bool = False,
-    prior_k0: float = 0.0,
-    prob_conv_sss_tol: float = 0.001,
-    prob_cred_set: float = 0.95,
-    pvalue_snps: float = 1.0,
-    rsids: Optional[List[str]] = None,
-    std_effects: bool = False,
-) -> None:
+    temp_dir: Optional[str] = None,
+) -> CredibleSet:
     """
-    Run FINEMAP with specified parameters.
+    Run FINEMAP with shotgun stochastic search.
 
     Parameters
     ----------
-    executable_path : str
-        Path to the FINEMAP executable.
-    in_files : str
-        Path to the master file.
-    subprogram : str, optional
-        Subprogram to run ('--cond', '--config', or '--sss') (default: '--sss').
-    cond_pvalue : float, optional
-        P-value threshold for declaring genome-wide significance (default: 5e-8).
-    corr_config : float, optional
-        Correlation threshold for causal configuration (default: 0.95).
-    force_n_samples : bool, optional
-        Allow correlations with different sample size than GWAS (default: False).
-    n_causal_snps : int, optional
-        Maximum number of allowed causal SNPs (default: 5).
-    n_configs_top : int, optional
-        Number of top causal configurations to save (default: 50000).
-    n_conv_sss : int, optional
-        Number of iterations for convergence in SSS (default: 100).
+    input : FmInput
+        Input data.
+    max_causal : int, optional
+        Maximum number of causal variants, by default 1, only support 1.
+    coverage : float, optional
+        Coverage of the credible set, by default 0.95.
     n_iter : int, optional
-        Maximum number of iterations for SSS (default: 100000).
+        Number of iterations, by default 100000.
     n_threads : int, optional
-        Number of parallel threads (default: 1).
-    prior_k : bool, optional
-        Use prior probabilities for number of causal SNPs (default: False).
-    prior_k0 : float, optional
-        Prior probability of no causal SNP (default: 0.0).
-    prob_conv_sss_tol : float, optional
-        Tolerance for SSS convergence (default: 0.001).
-    prob_cred_set : float, optional
-        Probability for credible interval (default: 0.95).
-    pvalue_snps : float, optional
-        P-value threshold for including SNPs (default: 1.0).
-    rsids : List[str], optional
-        List of SNP identifiers (for --config subprogram).
-    std_effects : bool, optional
-        Print mean and std dev of posterior effect size distribution (default: False).
+        Number of threads, by default 1.
+    temp_dir : Optional[str], optional
+        Temporary directory, by default None.
 
     Returns
     -------
-    subprocess.CompletedProcess
-        Result of the subprocess run.
+    CredibleSet
+        Credible set.
 
-    Examples
-    --------
-    >>> result = run_finemap(
-    ...     executable_path="/path/to/finemap",
-    ...     in_files="master_file.txt",
-    ...     subprogram="--sss",
-    ...     n_threads=4,
-    ...     n_causal_snps=10
-    ... )
-    >>> print(f"FINEMAP exited with return code {result.returncode}")
-    >>> print(f"FINEMAP output: {result.stdout}")
     """
-    # command = [executable_path, subprogram, "--in-files", in_files]
+    logger.info(f"Running FINEMAP for {input.prefix} with max_causal={max_causal}")
+    logger.info(f"Coverage: {coverage}, n_iter: {n_iter}, n_threads: {n_threads}")
+    # write z file
+    if ColName.MAF not in input.sumstats.columns:
+        raise ValueError(f"{ColName.MAF} is required for FINEMAP.")
+    finemap_input = input.sumstats.copy()
+    finemap_input[ColName.MAF] = finemap_input[ColName.MAF].replace(0, 0.00001)
+    finemap_input = finemap_input[
+        [
+            ColName.SNPID,
+            ColName.CHR,
+            ColName.BP,
+            ColName.EA,
+            ColName.NEA,
+            ColName.MAF,
+            ColName.BETA,
+            ColName.SE,
+        ]
+    ]
+    finemap_input.rename(
+        columns={
+            ColName.SNPID: "rsid",
+            ColName.CHR: "chromosome",
+            ColName.BP: "position",
+            ColName.MAF: "maf",
+            ColName.BETA: "beta",
+            ColName.SE: "se",
+            ColName.EA: "allele1",
+            ColName.NEA: "allele2",
+        },
+        inplace=True,
+    )
+    finemap_input.to_csv(f"{temp_dir}/finemap.z", sep=" ", index=False, float_format="%0.5f")
 
-    # Add other parameters
-    # command.extend(
-    #     [
-    #         "--cond-pvalue",
-    #         str(cond_pvalue),
-    #         "--corr-config",
-    #         str(corr_config),
-    #         "--n-causal-snps",
-    #         str(n_causal_snps),
-    #         "--n-configs-top",
-    #         str(n_configs_top),
-    #         "--n-conv-sss",
-    #         str(n_conv_sss),
-    #         "--n-iter",
-    #         str(n_iter),
-    #         "--n-threads",
-    #         str(n_threads),
-    #         "--prior-k0",
-    #         str(prior_k0),
-    #         "--prob-conv-sss-tol",
-    #         str(prob_conv_sss_tol),
-    #         "--prob-cred-set",
-    #         str(prob_cred_set),
-    #         "--pvalue-snps",
-    #         str(pvalue_snps),
-    #     ]
-    # )
+    # write ld file
+    np.savetxt(f"{temp_dir}/finemap.ld", input.r, delimiter=" ")  # TODO: write ld file only once for multiple tools
 
-    # if force_n_samples:
-    #     command.append("--force-n-samples")
+    # write master file
+    with open(f"{temp_dir}/finemap.master", "w") as f:
+        master_content = [
+            f"{temp_dir}/finemap.z",
+            f"{temp_dir}/finemap.ld",
+            f"{temp_dir}/finemap.snp",
+            f"{temp_dir}/finemap.config",
+            f"{temp_dir}/finemap.cred",
+            f"{temp_dir}/finemap.log",
+            str(input.sample_size),
+        ]
+        f.write("z;ld;snp;config;cred;log;n_samples\n")
+        f.write(";".join(master_content))
 
-    # if prior_k:
-    #     command.append("--prior-k")
-    # if prior_snps:
-    #     command.append("--prior-snps")
-    # if isinstance(prior_std, list):
-    #     command.extend(["--prior-std", ",".join(map(str, prior_std))])
-    # else:
-    #     command.extend(["--prior-std", str(prior_std)])
-    # if rsids:
-    #     command.extend(["--rsids", ",".join(rsids)])
-    # if std_effects:
-    #     command.append("--std-effects")
+    # run finemap
+    cmd = [
+        "--sss",
+        "--in-files",
+        f"{temp_dir}/finemap.master",
+        "--n-causal-snps",
+        str(max_causal),
+        "--n-iterations",
+        str(n_iter),
+        "--n-threads",
+        str(n_threads),
+        "--prob-cred-set",
+        str(coverage),
+    ]
+    required_output_files = [f"{temp_dir}/finemap.snp", f"{temp_dir}/finemap.config"]
+    tool_manager.run_tool("finemap", cmd, required_output_files)
 
-    # Run FINEMAP
-    # result = subprocess.run(command, capture_output=True, text=True, check=True)
-    # return result
-    pass
+    # get PIPs
+    if os.path.getsize(f"{temp_dir}/finemap.snp") == 0:
+        logger.warning("FINEMAP output is empty.")
+        pip = pd.Series(index=finemap_input["rsid"].values.tolist())
+    else:
+        finemap_res = pd.read_csv(f"{temp_dir}/finemap.snp", sep=" ", usecols=["rsid", "prob"])
+        finemap_res = pd.Series(finemap_res["prob"].values, index=finemap_res["rsid"].values)  # type: ignore
+        pip = finemap_res
+
+    # get credible set
+    if os.path.getsize(f"{temp_dir}/finemap.config") == 0:
+        logger.warning("FINEMAP output is empty.")
+        # cs_snps = []
+        # lead_snps = None
+        no_cred = True
+    else:
+        finemap_config = pd.read_csv(f"{temp_dir}/finemap.config", sep=" ", usecols=["config", "prob"])
+        finemap_config = finemap_config.sort_values("prob", ascending=False)
+        finemap_config = finemap_config[finemap_config["prob"].shift().fillna(0).cumsum() <= coverage]
+        cs_snps = list(set(finemap_config["config"].str.cat(sep=",").split(",")))
+        lead_snps = str(
+            input.sumstats.loc[
+                input.sumstats[input.sumstats[ColName.SNPID].isin(cs_snps)][ColName.P].idxmin(), ColName.SNPID
+            ]
+        )
+
+    # output
+    logger.info(f"Fished FINEMAP for {input.prefix}")
+    logger.info("N of credible set: 1")
+    logger.info(f"Credible set size: {len(cs_snps)}")
+    return CredibleSet(
+        tool="FINEMAP",
+        n_cs=1,
+        coverage=coverage,
+        lead_snps=[lead_snps] if not no_cred else [],
+        snps=[cs_snps] if not no_cred else [],
+        cs_sizes=[len(cs_snps)] if not no_cred else [],
+        pips=pip,
+        parameters={"max_causal": max_causal, "n_iter": n_iter, "n_threads": n_threads},
+    )
+
+
+def run_finemap_multi(
+    inputs: list[FmInput],
+    max_causal: int = 1,
+    coverage: float = 0.95,
+    n_iter: int = 100000,
+    n_threads: int = 1,
+    combine_cred: str = "union",
+    combine_pip: str = "max",
+    jaccard_threshold: float = 0.1,
+) -> CredibleSet:
+    """
+    Run FINEMAP for multiple datasets.
+
+    Run FINEMAP for each dataset in the input list. Then combine the results.
+
+    Parameters
+    ----------
+    inputs : list[FmInput]
+        List of input data.
+    max_causal : int, optional
+        Maximum number of causal variants, by default 1, only support 1.
+    coverage : float, optional
+        Coverage, by default 0.95.
+    n_iter : int, optional
+        Number of iterations, by default 100000.
+    n_threads : int, optional
+        Number of threads, by default 1.
+    combine_cred : str, optional
+        Method to combine credible sets, by default "union".
+    combine_pip : str, optional
+        Method to combine PIPs, by default "max".
+    jaccard_threshold : float, optional
+        Jaccard index threshold for merging credible sets, by default 0.1.
+
+    Returns
+    -------
+    CredibleSet
+        Combined credible set.
+
+    """
+    logger.info("Running FINEMAP for multiple datasets.")
+    logger.info(f"max_causal={max_causal}, coverage={coverage}, n_iter={n_iter}, n_threads={n_threads}")
+    logger.info(f"combine_cred={combine_cred}, combine_pip={combine_pip}, jaccard_threshold={jaccard_threshold}")
+
+    # run FINEMAP for each dataset
+    cs_list = []
+    for input in inputs:
+        cs = run_finemap_single(input, max_causal, coverage, n_iter, n_threads)
+        cs_list.append(cs)
+
+    # combine credible sets
+    combined_cs = combine_creds(cs_list, combine_cred, combine_pip, jaccard_threshold)
+    logger.info("Finished FINEMAP for multiple datasets.")
+    return combined_cs
