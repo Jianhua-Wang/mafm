4f91a6746d0948093fcf3f337eda2c1821350718
Jianhua-Wang <jianhua.mert@gmail.com>
2024-12-16 00:11:36 -0500

    Bump version: 0.0.13 → 0.0.14

--- mafm/__init__.py
+++ mafm/__init__.py
@@ -6,7 +6,7 @@ from rich.logging import RichHandler
 
 __author__ = """Jianhua Wang"""
 __email__ = "jianhua.mert@gmail.com"
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 
 
 logging.basicConfig(
